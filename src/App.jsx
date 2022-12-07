4cb07a8ead73f4ee3a62e27d7128b4e28ba3434f
PSAiridas <videikisairidas@gmail.com>
2022-12-07 13:41:55 +0200

    v0.1.1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './assets/scss/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <div>
+            <ul>
+                <li><a href="react.airidasvideikis.me">React </a></li>
+                <li><a href="">Vue </a></li>
+                <li><a href="">Angular </a></li>
+            </ul>
+            <ul>
+                <li>
+                    </li>
+
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default App
