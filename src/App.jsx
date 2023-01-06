68f51e47497341047db14baad257fcbf62f5dab9
PSAiridas <videikisairidas@gmail.com>
2023-01-06 23:38:41 +0200

    v0.0.6test1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>v0.0.6</div>
+      <div>v0.0.6test1</div>
       <div>
           <ul>
               <li><a href="react.airidasvideikis.me">React </a></li>
